c3a690350fa2c72f84fd66d88c119c8636ad9e6f
nagaranit <39076590+nagaranit@users.noreply.github.com>
2018-05-24 09:03:40 +0530

    returning html string

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -2,4 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def main_page(request):
-    return HttpResponse('Welcome to Django bookmarks')
+    output = '''
+    <html>
+    <head><title>%s</title></head>
+    <body>
+    <h1>%s</h1>
+    <p>%s</p>
+    </body>
+    </html>''' % (
+        'Django Bookmarks',
+        'Welcome to Django Bookmarks',
+        'where you can store and share bookmarks!')
+
+    return HttpResponse (output)
+
