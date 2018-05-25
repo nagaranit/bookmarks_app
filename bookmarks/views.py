a3ee9cf2572bd5d6aa0f9b35659608348ba1a08b
nagaranit <39076590+nagaranit@users.noreply.github.com>
2018-05-25 08:33:16 +0530

    seperated ui code from view to html

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,18 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-def main_page(request):
-    output = '''
-    <html>
-    <head><title>%s</title></head>
-    <body>
-    <h1>%s</h1>
-    <p>%s</p>
-    </body>
-    </html>''' % (
-        'Django Bookmarks',
-        'Welcome to Django Bookmarks',
-        'where you can store and share bookmarks!')
 
-    return HttpResponse (output)
+
+def main_page (request): 
+    template =loader.get_template('bookmarks/main_page.html')
+    return HttpResponse(template.render({}, request))
 
